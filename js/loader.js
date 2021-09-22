36acb4b720f299e650960e60e5a6903c18aea116
solomon-ayodele65 <solomonayodele65@gmail.com>
2021-09-22 16:06:41 +0100

    updates

--- js/loader.js
+++ js/loader.js
@@ -5,6 +5,7 @@ var loader= document.getElementById('loader')
 setTimeout(()=>{
     loader.style.transition="5s",
     loader.style.display="none",
+    // page.style.visibility="visible"
     // page.style.opacity="1"
     page.style.display="block"
     },10000)
